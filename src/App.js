cab63eb7db181ace9d04ba14b0522e05d5939802
Alex Brown <alexdbrown2526@gmail.com>
2022-01-26 21:06:44 -0600

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Landing from "./pages/Landing";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/landing">Landing</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<div>error</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
-import styled from "styled-components";
+import Wrapper from "../wrappers/LandingWrapper";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -15,48 +16,12 @@ const Landing = () => {
           normcore chartreuse post-ironic wayfarers. Cloud bread cold-pressed
           cronut succulents lo-fi raw denim.
         </p>
-        <button>login/register</button>
+        <Link to="/register" className="'btn btn'hero">
+          Login/register
+        </Link>
       </div>
     </Wrapper>
   );
 };
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-    p {
-      color: var(--grey-600);
-    }
-    .main-img {
-      display: none;
-    }
-    @media (min-width: 992px) {
-      .page {
-        grid-template-columns: 1fr 1fr;
-        column-gap: 3rem;
-      }
-      .main-img {
-        display: block;
-      }
-    }
-  }
-`;
 
 export default Landing;
