bcf89e831d609d2fe192712c6c0a82b62c4808d8
Alex Brown <alexdbrown2526@gmail.com>
2022-01-26 21:16:41 -0600

    setup nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import Landing from "./pages/Landing";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+//import { Dashboard, Register, Error, Landing } from "./pages";
+import Dashboard from "./pages/Dashboard";
+import Error from "./pages/Error";
+import Landing from "./pages/Landing";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Dashboard</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/landing">Landing</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
-        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
-        <Route path="*" element={<div>error</div>} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Dashboard from "./Dashboard";
+import Register from "./Register";
+import Error from "./Error";
+import Landing from "./Landing";
+
+export default {
+  Dashboard,
+  Landing,
+  Error,
+  Register,
+};
