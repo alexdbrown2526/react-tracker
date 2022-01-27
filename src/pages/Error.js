27581af77911ce0e1f94520626316611f4b4969e
Alex Brown <alexdbrown2526@gmail.com>
2022-01-26 21:45:40 -0600

    setup error page for wrong route

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import Wrapper from "../wrappers/ErrorPage";
+
 const Error = () => {
-  return <div>error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <h3>Page Not Found</h3>
+        <Link to="/">Return Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
