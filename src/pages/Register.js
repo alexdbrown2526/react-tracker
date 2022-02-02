b5bd94480b25853a6f773bb045ce4213a58c581d
Alex Brown <alexdbrown2526@gmail.com>
2022-02-01 18:46:02 -0600

    setup inital global state and begin register form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={values.name} onChange={handleChange}></input>
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
