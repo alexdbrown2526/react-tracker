b3b9a8d6b367739663786fb2aee6cf308e73fd71
Alex Brown <alexdbrown2526@gmail.com>
2022-02-15 20:04:57 -0600

    clear alert functionality after 2 seconds of incomplete form

--- src/Context/reducers.js
+++ src/Context/reducers.js
@@ -1,5 +1,5 @@
 import React, { useReducer, useContext } from "react";
-import { DISPLAY_ALERT } from "./actions";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 
 const reducer = (state, action) => {
   if (action.type === DISPLAY_ALERT) {
@@ -8,6 +8,14 @@ const reducer = (state, action) => {
       showAlert: true,
       alertType: "danger",
       alertText: "Please fill all fields",
+    };
+  }
+  if (action.type === CLEAR_ALERT) {
+    return {
+      ...state,
+      showAlert: false,
+      alertType: "",
+      alertText: "",
     };
   }
   throw new Error(`no such action ${action.type}`);
