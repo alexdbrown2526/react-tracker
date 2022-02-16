3e30eacd8849e605ba50ebe0a7fbeccdd4b7a584
Alex Brown <alexdbrown2526@gmail.com>
2022-02-15 20:26:50 -0600

    server setup and running

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+app.get("/", (req, res) => {
+  throw new Error("error");
+  res.send("hello");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`${port}`);
+});
