c68a93d6c66e7f953769990a64444864099fff82
Alex Brown <alexdbrown2526@gmail.com>
2022-02-17 22:26:26 -0600

    connected to db mongo, setup through atlas

--- server.js
+++ server.js
@@ -3,19 +3,29 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+import connectDB from "./db/connect.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
-app.get("/", (req, res) => {
-  throw new Error("error");
-  res.send("hello");
-});
+// app.get("/", (req, res) => {
+//   throw new Error("error");
+//   res.send("hello");
+// });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`running on ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
