537daac5518de88423529e98162b89fea4c98a1f
Alex Brown <alexdbrown2526@gmail.com>
2022-02-22 21:37:32 -0600

    start creating controllers/routes

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import connectDB from "./db/connect.js";
+
+import authRouter from "./routes/authRoutes.js";
+
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
-// app.get("/", (req, res) => {
-//   throw new Error("error");
-//   res.send("hello");
-// });
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
